52f088450d7112896606ed17b0125140caecd840
Patrice Hermenault <hermenaultpatrice@gmail.com>
2021-10-05 18:18:17 +0100

    Updated backend routing to make app deployable to Heroku

--- sg-frontend/src/services/sessions.js
+++ sg-frontend/src/services/sessions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/sessions'
+const baseUrl = '/api/sessions'
 
 const fetchSessions = async (user) => {
     const response = await axios.get(`${baseUrl}/${user}`)
--- sg-frontend/src/services/users.js
+++ sg-frontend/src/services/users.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/users'
+const baseUrl = '/api/users'
 
 const createUser = async (newUser) => {
     const response = await axios.post(baseUrl, newUser)
