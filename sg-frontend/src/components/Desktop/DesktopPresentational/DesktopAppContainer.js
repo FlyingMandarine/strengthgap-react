f202fd960f05c68b9be5ebe7ebdb307ac211582d
Patrice Hermenault <hermenaultpatrice@gmail.com>
2021-10-20 15:04:36 +0100

    Adjusted viewport to allow for scrolling on mobile

--- sg-frontend/src/components/Desktop/DesktopPresentational/DesktopAppContainer.js
+++ sg-frontend/src/components/Desktop/DesktopPresentational/DesktopAppContainer.js
@@ -2,21 +2,26 @@ import React from 'react'
 
 const DesktopAppContainer = (props) => {
 
+    const outerDivStyle = {
+        display: 'flex',
+        position: 'relative',
+        height: '100vh',
+    }
+
     const divStyle = {
         backgroundColor: 'white',
         display: 'flex',
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-
+        position: 'relative',
         border: 'solid 3px black',
         borderRadius: 6,
+        margin: 'auto',
     }
 
     return (
-        <div style={ divStyle } className='desktop-container'>
-            { props.children }
+        <div style={ outerDivStyle }>
+            <div style={ divStyle } className='desktop-container'>
+                { props.children }
+            </div>
         </div>
     )
 }
--- sg-frontend/src/components/Mobile/MobilePresentational/MobileAppContainer.js
+++ sg-frontend/src/components/Mobile/MobilePresentational/MobileAppContainer.js
@@ -2,25 +2,31 @@ import React from 'react'
 
 const MobileAppContainer = (props) => {
 
+    const outerDivStyle = {
+        display: 'flex',
+        position: 'relative',
+        height: '100vh',
+    }
+
     const divStyle = {
         backgroundColor: 'white',
         display: 'flex',
         flexDirection: 'column',
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
+        position: 'relative',
 
         height: 857,
         width: 390,
 
         border: 'solid 3px black',
-        borderRadius: 6
+        borderRadius: 6,
+        margin: 'auto',
     }
 
     return (
-        <div style={ divStyle }>
-            { props.children }
+        <div style={ outerDivStyle }>
+            <div style={ divStyle }>
+                { props.children }
+            </div>
         </div>
     )
 }
